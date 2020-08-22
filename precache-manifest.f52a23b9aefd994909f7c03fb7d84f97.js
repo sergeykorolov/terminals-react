d4986d665ae0172d14186ad38f8c75a265ec0d12
sergeyorolov <korolovsergio@mail.ru>
2020-08-22 21:23:51 +0300

    Updates

---
+++ precache-manifest.f52a23b9aefd994909f7c03fb7d84f97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cc4e80dd52a668c766e8896f0347f5",
+    "url": "/terminals-react/index.html"
+  },
+  {
+    "revision": "f40dd10b5212179e3425",
+    "url": "/terminals-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b4f06c195827d2f6be86",
+    "url": "/terminals-react/static/css/main.5f240fe5.chunk.css"
+  },
+  {
+    "revision": "f40dd10b5212179e3425",
+    "url": "/terminals-react/static/js/2.bede81b5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/terminals-react/static/js/2.bede81b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f06c195827d2f6be86",
+    "url": "/terminals-react/static/js/main.0ff88d6d.chunk.js"
+  },
+  {
+    "revision": "ff54c75475f5673a6845",
+    "url": "/terminals-react/static/js/runtime-main.9848f4bf.js"
+  }
+]);
